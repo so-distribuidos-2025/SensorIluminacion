baf518f44b36b3e4f18bcfef554885cce1d5e601
anacautalan-creator <ana.c.autalan@gmail.com>
2025-08-28 19:03:49 -0300

    Update SensorIluminacion.java
    Modificado para conectarse con el servidor

--- SensorIluminacion/src/main/java/com/mycompany/sensoriluminacion/SensorIluminacion.java
+++ SensorIluminacion/src/main/java/com/mycompany/sensoriluminacion/SensorIluminacion.java
@@ -13,7 +13,24 @@ public class SensorIluminacion {
     public static void main(String[] args) {
         //System.out.println("Hello World!");
         
-        HiloSensor sensor = new HiloSensor();
-        sensor.run();
+        /*HiloSensor sensor = new HiloSensor();
+        sensor.run();*/
+        InetAddress ipServidor = null;
+        PrintWriter pw;
+        try {
+            ipServidor = InetAddress.getByName("localhost");
+            Socket cliente = new Socket(ipServidor, 20000);
+            System.out.println(cliente);
+            pw = new PrintWriter(cliente.getOutputStream(), true); //El segundo parametro activa el autoflush para escribir en el buffer
+            pw.println("iluminacion");
+            HiloSensor sensor = new HiloSensor(cliente, pw);
+            sensor.encender();
+            sensor.start();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        
     }
 }
